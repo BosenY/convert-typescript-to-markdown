8f25dd641c7e3a214b0d780b19107cda3187ce96
Boseny <yxy43445@gmail.com>
2022-07-06 00:28:15 +0800

    Add github fork

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,25 @@
 import React, { ReactNode } from "react";
 import Header from "./Header";
 import Footer from './Footer'
+import GitHubForkRibbon from 'react-github-fork-ribbon';
 import xw from 'xwind'
 type Props = {
   children: ReactNode;
 };
 
 const Layout: React.FC<Props> = (props) => (
-  <div css={xw`flex flex-col h-screen`}>
-    <Header />
-    <div css={xw`flex-1`}>{props.children}</div>
-    <Footer />
-  </div>
+  <>
+    <GitHubForkRibbon href="https://github.com/BosenY/convert-typescript-to-markdown"
+      target="_blank"
+      position="right">
+      Fork me on GitHub
+    </GitHubForkRibbon>
+    <div css={xw`flex flex-col h-screen`}>
+      <Header />
+      <div css={xw`flex-1`}>{props.children}</div>
+      <Footer />
+    </div>
+  </>
 );
 
 export default Layout;
